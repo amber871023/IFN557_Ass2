7541fdf9d22bee4a010ef049d3db630b12305e2f
amber871023 <amber871023@gmail.com>
2025-03-18 15:15:57 +1000

    Prepare for Vercel deployment

--- run.py
+++ run.py
@@ -1,6 +1,17 @@
+# from mystoreapp import create_app
+
+# if __name__ == '__main__':
+#     napp = create_app()
+#     napp.run(debug=True)
+
 from mystoreapp import create_app
 
-if __name__ == '__main__':
-    napp = create_app()
-    napp.run(debug=True)
-    
+# Create the Flask app instance
+app = create_app()
+
+# Vercel requires a `handler` function
+def handler(request, *args, **kwargs):
+    return app(request.environ, start_response)
+
+if __name__ == "__main__":
+    app.run(debug=True)
